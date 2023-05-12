aa711537062eb6433c560de67dbf77ad2fa97e55
AbhayGunputh <kumarabhay150499@gmail.com>
2023-05-13 00:53:03 +0400

    Code for TP2

---
+++ TP2_PMH/mdc_100_series/lib/login.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            const SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/diamond.png'),
+                const SizedBox(height: 16.0),
+                const Text('SHRINE'),
+              ],
+            ),
+            const SizedBox(height: 120.0),
+            // TODO: Remove filled: true values (103)
+            // TODO: Add TextField widgets (101)
+            // [Name]
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            // spacer
+            SizedBox(height: 12.0),
+            // [Password]
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            // TODO: Add button bar (101)
+            ButtonBar(
+            // TODO: Add a beveled rectangular border to CANCEL (103)
+            children: <Widget>[
+              // TODO: Add buttons (101)
+              TextButton(
+                child: Text('CANCEL'),
+                onPressed: () {
+                  // TODO: Clear the text fields (101)
+                  _usernameController.clear();
+                  _passwordController.clear();
+                },
+              ),
+              // TODO: Add an elevation to NEXT (103)
+              // TODO: Add a beveled rectangular border to NEXT (103)
+              ElevatedButton(
+                child: Text('NEXT'),
+                onPressed: () {
+              // TODO: Show the next page (101) 
+                ElevatedButton(
+                  child: Text('NEXT'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                );
+                },
+              ),
+          ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
